5342673eb2087a0c9906d5a2ca2b959fc98a9958
nj <nj@njs-MacBook-Pro.local>
2020-04-21 16:14:46 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         back to original but no!
         </p>
         <a
           className="App-link"
